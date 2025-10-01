3b4f4243d6251573cd0304907f1246581bba61f4
tugaincdev <tugaincdev@gmail.com>
2025-10-01 17:57:44 +0100

    Create Comando.java

---
+++ Comando.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Comando {
+	
+	
+	private int arg1, arg2, indent;
+	public static int ID_Reta = 1;
+	public static int ID_Parar = 0;
+	
+	
+	
+	
+	public Comando( int argID, int arg1, int arg2) {
+		
+		this.indent = argID;
+		this.arg1 = arg1;
+		this.arg2 = arg2;
+		
+		
+		
+	}
+    public Comando( int argID, int arg1) {
+		
+		this.indent = argID;
+		this.arg1 = arg1;
+		
+		
+		
+	}
+
+}
