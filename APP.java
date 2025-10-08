d1f5cf8cafa7af0d1100bc4806ce98fb7587fb70
tugaincdev <tugaincdev@gmail.com>
2025-10-08 16:50:18 +0100

    Update APP.java

--- APP.java
+++ APP.java
@@ -3,14 +3,20 @@ import javax.swing.SwingUtilities;
 public class App  extends Thread{
     private GUI_TP1 gui;
     private Dados dados;
+    private MovimentosAleatorios movimentosAl;
 
     public App() {
         
         gui = new GUI_TP1();
         
         dados = new Dados(gui);
+        
+        movimentosAl = new MovimentosAleatorios();
        
         gui.setDados(dados);
+        
+        movimentosAl.setDados(dados);
+        
         
     }
 
