ad66e2521bf6267653a42f1c9058308d3e727277
Mister-Lad <44650020+Mister-Lad@users.noreply.github.com>
2025-10-08 16:51:00 +0100

    Update BufferCircularM.java

--- BufferCircularM.java
+++ BufferCircularM.java
@@ -4,35 +4,49 @@ import java.util.concurrent.Semaphore;
 public class BufferCircularM {
   final int dimBuffer = 16;
   private Semaphore livres, ocupados, acesso;
-  private Comando buffer;
+  private Comando[] buffer;
   private int putBuffer, getBuffer;
   
-  public BufferCircular() {
+  public BufferCircularM() {
   
     livres = new Semaphore(dimBuffer);
     ocupados = new Semaphore(0);
     acesso = new Semaphore(1);
-    buffer = new Comando()[dimBuffer]; //maybe wrong
+    buffer = new Comando[dimBuffer]; //maybe wrong
   }
 
   public void escrever(Comando c) {
-    
-    livres.acquire();
-    acesso.acquire();
-    buffer[putBuffer] = c;
-    putBuffer = ++putBuffer % dimBuffer;
+    try {
+    	livres.acquire();
+        acesso.acquire();
+        buffer[putBuffer] = c;
+        putBuffer = ++putBuffer % dimBuffer;
+    } catch (InterruptedException e) {
+    	Thread.currentThread().interrupt();
+        System.err.println("Thread interrupted while writing to buffer: " + e.getMessage());
+    }
     acesso.release();
     ocupados.release();
   }
 
-  public String ler() {
-
-    Comando c;
-    ocupados.acquire();
-    acesso.acquire();
-    c = buffer[getBuffer];
-    acesso.release();
-    livres.release();
-    return c;
+  public Comando ler() {
+	Comando c = null; //tive de por para n dar erro
+	try {	  
+		ocupados.acquire();
+		acesso.acquire();
+	} catch(InterruptedException e) {
+    	Thread.currentThread().interrupt();
+        System.err.println("Thread interrupted while writing to buffer: " + e.getMessage());
+	}
+	acesso.release();
+	livres.release();
+	return c;
+  }
+  
+  public void inserirElemento(Comando c) {
+  	
+  	
+  	
+  	
   }
 }
