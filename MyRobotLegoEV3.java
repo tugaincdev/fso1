6a9f437e0677945a336ccfe05e87ade502385b04
tugaincdev <tugaincdev@gmail.com>
2025-10-08 17:48:41 +0100

    Update MyRobotLegoEV3.java

--- MyRobotLegoEV3.java
+++ MyRobotLegoEV3.java
@@ -6,34 +6,39 @@ public class MyRobotLegoEV3  extends Thread {
 	boolean movAlF = false;
 	
 	public MyRobotLegoEV3(){
-		
-		
+		buffer = new BufferCircularM();
+		robot = new RobotLegoEV3();	
 	}
-	
 	
 	
 	public void Reta (int distancia) {
 		Comando c = new Comando(Comando.ID_Reta, distancia);
+		buffer.escrever(c);
 		
 	}
-	public void CurvarEsquerda (int angulo, int raio ) {
+	public void CurvarEsquerda(int angulo, int raio ) {
 		Comando c = new Comando(Comando.ID_CurvarEsquerda, angulo, raio);
+		buffer.escrever(c);
 		
 	}
-	public void CurvarDireita (int angulo, int raio ) {
+	public void CurvarDireita(int angulo, int raio ) {
 		Comando c = new Comando(Comando.ID_CurvarDireita, angulo, raio);
+		buffer.escrever(c);
 		
 	}
-	public void OpenEV3(String nome) {
-		Comando c = new Comando(Comando.ID_OpenEV3, nome);	
+	public boolean OpenEV3(String nome) {
+		return(robot.OpenEV3(nome));
 	}
 	
 	public void Parar (boolean con) {
 		Comando c = new Comando(Comando.ID_Parar, con);	
+		buffer.escrever(c);
 	}
 	public void CloseEV3 () {
-		Comando c = new Comando(Comando.ID_CloseEV3);	
+		robot.CloseEV3();
+		
 	}
+	
 	
 
 	
@@ -44,6 +49,12 @@ public class MyRobotLegoEV3  extends Thread {
 	
 	
 	 public void run() {
+		 
+		 
+		 
+		 
+		 
+		 
 	    	
 	    }
 
