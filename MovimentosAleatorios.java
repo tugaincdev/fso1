6f210a5b303cafa44f7fef049a540566bb94bfeb
tugaincdev <tugaincdev@gmail.com>
2025-10-08 17:07:20 +0100

    Update MovimentosAleatorios.java

--- MovimentosAleatorios.java
+++ MovimentosAleatorios.java
@@ -1,4 +1,33 @@
+import java.util.ArrayList;
 
-public class MovimentosAleatorios {
+public class MovimentosAleatorios extends Thread  {
+	private Dados dados;
+	
+	
+	
+public MovimentosAleatorios() {
+		
+	
+		
+	}
+
+
+public void run() {
+	
+	
+	
+	
+	
+}
+
+	
+	public void setDados(Dados dados) {
+		this.dados = dados;
+	}
+	public Dados getDados() {
+		return dados;
+	}
+	
+	
 
 }
