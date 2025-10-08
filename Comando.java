58b8510e3c0f1f22d46dfdf29772366a9e9097be
Mister-Lad <44650020+Mister-Lad@users.noreply.github.com>
2025-10-08 16:58:31 +0100

    Update Comando.java
    openEV3 code, also bool and string args

--- Comando.java
+++ Comando.java
@@ -4,11 +4,14 @@ public class Comando {
 	
 	
 	private int arg1, arg2, indent;
+	private boolean arg1B;
+	private String arg1S;
 	public static int ID_Reta = 1;
 	public static int ID_CurvarEsquerda = 2;
 	public static int ID_CurvarDireita = 3;
 	public static int ID_Parar = 0;
-	public static int ID_Ligar = 4;
+	public static int ID_LigarID = 4;
+	public static int ID_OpenEV3 = 5;
 
 	
 	
@@ -23,10 +26,28 @@ public class Comando {
 		
 		
 	}
-    public Comando( int argID, int arg1) {
+    public Comando(int argID, int arg1) {
 		
 		this.indent = argID;
 		this.arg1 = arg1;
+		
+		
+		
+	}
+    
+    public Comando(int argID, boolean arg1) {
+		
+		this.indent = argID;
+		this.arg1B = arg1;
+		
+		
+		
+	}
+    
+    public Comando(int argID, String arg1) {
+		
+		this.indent = argID;
+		this.arg1S = arg1;
 		
 		
 		
