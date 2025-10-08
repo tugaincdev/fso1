f2adea29d7279855af92971251da8d9d16dffcb5
Mister-Lad <44650020+Mister-Lad@users.noreply.github.com>
2025-10-08 18:34:09 +0100

    Update MyRobotLegoEV3.java
    siete, ocho, nueve

--- MyRobotLegoEV3.java
+++ MyRobotLegoEV3.java
@@ -50,12 +50,49 @@ public class MyRobotLegoEV3  extends Thread {
 	
 	 public void run() {
 		 
-		 
-		 
-		 
-		 
-		 
-	    	
-	    }
 
+		
+		try {
+			Comando c = buffer.ler();
+			
+			
+			
+			//public static int ID_Reta = 1;
+			//public static int ID_CurvarEsquerda = 2;
+			//public static int ID_CurvarDireita = 3;
+			//public static int ID_Parar = 0;
+			//public static int ID_LigarID = 4;
+			//public static int ID_OpenEV3 = 5;
+			//public static int ID_CloseEV3 = 6;
+			
+			
+			
+			
+			switch (c.getArgID()) {
+				case 1:
+					robot.Reta(c.getArg1());
+					long tempoReta = (long) (Math.abs(c.getArg1()) / VEL_ROBOT * 1000) + 100;
+					Thread.sleep(tempoReta);
+					break;
+				case 2:
+					robot.CurvarEsquerda(c.getArg1(), c.getArg2());
+					long tempoEsq = (long) (c.getArg2() * Math.toRadians(c.getArg1()) / VEL_ROBOT * 1000) + 100;
+					Thread.sleep(tempoEsq);
+					break;
+				case 3:
+					robot.CurvarDireita(c.getArg1(), c.getArg2());
+					long tempoDir = (long) (c.getArg2() * Math.toRadians(c.getArg1()) / VEL_ROBOT * 1000) + 100;
+					Thread.sleep(tempoDir);
+					break;
+				case 0:
+					robot.Parar(c.getArg1B());
+					Thread.sleep(100);
+					break;	
+			} 
+			
+		} catch(InterruptedException e) {
+			 Thread.currentThread().interrupt(); 
+			 System.err.println("Thread interrupted while writing to buffer: " + e.getMessage());
+		}	    	
+    }
 }
