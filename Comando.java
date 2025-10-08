5ac58317a7367744d68e65e628f92f77b88c60ac
Mister-Lad <44650020+Mister-Lad@users.noreply.github.com>
2025-10-08 16:50:19 +0100

    Update Comando.java
    all work (hopefully) from eclipse

--- Comando.java
+++ Comando.java
@@ -5,7 +5,11 @@ public class Comando {
 	
 	private int arg1, arg2, indent;
 	public static int ID_Reta = 1;
+	public static int ID_CurvarEsquerda = 2;
+	public static int ID_CurvarDireita = 3;
 	public static int ID_Parar = 0;
+	public static int ID_Ligar = 4;
+
 	
 	
 	
@@ -27,5 +31,7 @@ public class Comando {
 		
 		
 	}
+    
+    
 
 }
