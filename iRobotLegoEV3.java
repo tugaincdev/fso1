36ea65ce8a3efd5c864186e90b16ec7407d234f1
Mister-Lad <44650020+Mister-Lad@users.noreply.github.com>
2025-10-15 16:17:57 +0100

    Add files via upload

---
+++ iRobotLegoEV3.java
@@ -0,0 +1,16 @@
+
+public interface iRobotLegoEV3 {
+	
+	void Reta(int distancia);
+	
+	void CurvarDireita(int raio, int angulo);
+
+	void CurvarEsquerda(int raio, int angulo);
+	
+	void Parar(boolean b);
+	
+	boolean OpenEV3(String s);
+	
+	void CloseEV3();
+
+}
