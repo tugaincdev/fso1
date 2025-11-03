dd6da57fdbef7735962d85ed1aa832599f9617ef
tugaincdev <tugaincdev@gmail.com>
2025-11-03 15:02:18 +0000

    Update Dados.java

--- Dados.java
+++ Dados.java
@@ -1,120 +1,148 @@
-
-
 import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
 
 public class Dados {
-	private GUI_TP1 gui;
-	private String nomeRobot;
-	private ArrayList<String> texto;
-	private int raio, angulo, distancia, spinner;
-	private MyRobotLegoEV3 robot;
-	private boolean onOff, onOffAl, onOffSimulado;
-	
-	public Dados(GUI_TP1 g) {
-		
-		nomeRobot = "ZE";
-		gui = g;
-		texto = new ArrayList<String>();
-		raio = 20;
-		angulo = 20;
-		distancia = 20;
-		onOff = false;
-		robot = new MyRobotLegoEV3();
-		robot.start();
-		spinner = 1;
-		onOffAl = false;
-		onOffSimulado = false;
-		
-	}
-	
-	public boolean isOnOffAl() {
-		return onOff;
-	}
+    private GUI_TP1 gui;
+    private String nomeRobot;
+    private ArrayList<String> texto;
+    private int raio, angulo, distancia, spinner;
+    private MyRobotLegoEV3 robot;
+    private RobotLegoSimulado robot1;
+    private boolean onOff, onOffAl, onOffSimulado;
+    private Semaphore semaforoRobo = new Semaphore(1); 
+    
 
-	public void setOnOffAl(boolean onOffAl) {
-		this.onOffAl = onOffAl;
-	}
+    public Dados(GUI_TP1 g) {
 
-	
-	
-	public void setSpinner(int spinner) {
-		this.spinner = spinner;
-	}
-	
-	public int getSpinner() {
-		return spinner;
-	}
+        nomeRobot = "ZE";
+        gui = g;
+        texto = new ArrayList<String>();
+        raio = 20;
+        angulo = 20;
+        distancia = 20;
+        spinner = 1;
 
-	public GUI_TP1 getGui() {
-		return gui;
-	}
+        
+        robot = null;
+        robot1 = null;
 
-	public void setGui(GUI_TP1 gui) {
-		this.gui = gui;
-	}
+        onOff = false;
+        onOffAl = false;
+        onOffSimulado = false;
+    }
 
-	public String getNomeRobot() {
-		return nomeRobot;
-	}
+    
+    public Semaphore getSemaforoRobo() {
+        return semaforoRobo;
+    }
+    
+    public MyRobotLegoEV3 getRobot() {
+        if (robot == null) {
+            robot = new MyRobotLegoEV3();
+            robot.start();
+        }
+        return robot;
+    }
 
-	public void setNomeRobot(String nomeRobot) {
-		this.nomeRobot = nomeRobot;
-	}
+    // Cria o robô simulado apenas quando necessário
+    public RobotLegoSimulado getRobotSimulado() {
+        if (robot1 == null) {
+            robot1 = new RobotLegoSimulado();
+        }
+        return robot1;
+    }
 
-	public ArrayList<String> getTexto() {
-		return texto;
-	}
+    // Liberta o robô físico da memória
+    public void limparRobot() {
+        robot = null;
+    }
 
-	public void setTexto(ArrayList<String> texto) {
-		this.texto = texto;
-	}
+    // Liberta o robô simulado da memória
+    public void limparRobotSimulado() {
+        robot1 = null;
+    }
 
-	public int getRaio() {
-		return raio;
-	}
+    // ============================================================
+    // GETTERS & SETTERS
+    // ============================================================
 
-	public void setRaio(int raio) {
-		this.raio = raio;
-	}
+    public boolean isOnOffAl() {
+        return onOffAl;
+    }
 
-	public int getAngulo() {
-		return angulo;
-	}
+    public void setOnOffAl(boolean onOffAl) {
+        this.onOffAl = onOffAl;
+    }
 
-	public void setAngulo(int angulo) {
-		this.angulo = angulo;
-	}
+    public void setSpinner(int spinner) {
+        this.spinner = spinner;
+    }
 
-	public int getDistancia() {
-		return distancia;
-	}
+    public int getSpinner() {
+        return spinner;
+    }
 
-	public void setDistancia(int distancia) {
-		this.distancia = distancia;
-	}
+    public GUI_TP1 getGui() {
+        return gui;
+    }
 
-	public MyRobotLegoEV3 getRobot() {
-		return robot;
-	}
+    public void setGui(GUI_TP1 gui) {
+        this.gui = gui;
+    }
 
-	public void setRobot(MyRobotLegoEV3 robot) {
-		this.robot = robot;
-	}
-	
-	public boolean isOnOff() {
-		return onOff;
-	}
+    public String getNomeRobot() {
+        return nomeRobot;
+    }
 
-	public void setOnOff(boolean onOff) {
-		this.onOff = onOff;
-	}
-	
-	public boolean isOnOffSimulado() {
-		return onOffSimulado;
-	}
+    public void setNomeRobot(String nomeRobot) {
+        this.nomeRobot = nomeRobot;
+    }
 
-	public void setOnOffSimulado(boolean bob) {
-		this.onOffSimulado = bob;
-	}
+    public ArrayList<String> getTexto() {
+        return texto;
+    }
 
+    public void setTexto(ArrayList<String> texto) {
+        this.texto = texto;
+    }
+
+    public int getRaio() {
+        return raio;
+    }
+
+    public void setRaio(int raio) {
+        this.raio = raio;
+    }
+
+    public int getAngulo() {
+        return angulo;
+    }
+
+    public void setAngulo(int angulo) {
+        this.angulo = angulo;
+    }
+
+    public int getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(int distancia) {
+        this.distancia = distancia;
+    }
+
+    public boolean isOnOff() {
+        return onOff;
+    }
+
+    public void setOnOff(boolean onOff) {
+        this.onOff = onOff;
+    }
+
+    public boolean isOnOffSimulado() {
+        return onOffSimulado;
+    }
+
+    public void setOnOffSimulado(boolean onOffSimulado) {
+        this.onOffSimulado = onOffSimulado;
+    }
 }
