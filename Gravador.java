6b802eae2f075e91c9bf5d31e37707984c16d549
tugaincdev <tugaincdev@gmail.com>
2025-11-19 16:47:03 +0000

    Create Gravador.java

---
+++ Gravador.java
@@ -0,0 +1,32 @@
+public class Gravador extends Thread {
+	
+
+	
+
+
+	
+	public Gravador() {
+		
+	}
+	
+	
+	public void run() {
+		
+		
+    	while (true) {
+    		
+    	  System.out.println("EHEHEHE");
+            if (!dados.isOnOff()) {
+                continue; 
+            }
+            
+            
+            
+            
+    		
+    	}
+	}
+    		
+
+
+}
