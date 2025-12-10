e876572a61b53ac27c67b6d8d7771293166779c8
tugaincdev <tugaincdev@gmail.com>
2025-12-10 18:26:20 +0000

    Update MovimentosAleatorios.java

--- MovimentosAleatorios.java
+++ MovimentosAleatorios.java
@@ -1,150 +1,96 @@
-import java.util.ArrayList;
-
 public class MovimentosAleatorios extends Thread  {
-	private Dados dados;
-	private int oneTwoThree;
-	private int raio, distancia, angulo;
-	private static int VEL_ROBOT = 20;
+
+    private Dados dados;
+    private int oneTwoThree;
+    private int raio, distancia, angulo;
+    private static int VEL_ROBOT = 20;
+
+    private volatile Estado estado = Estado.NOT_ACTIVE;
     private volatile boolean running = true;
-	
-	
+
+    public MovimentosAleatorios() {
+    }
+
+    public void setDados(Dados data) {
+        this.dados = data;
+    }
+    public Estado getEstado() {
+        return estado;
+    }
+
+    /** PARA parar temporariamente (GUI desmarca checkbox) **/
     public void stopRunning() {
+        estado = Estado.NOT_ACTIVE;
+    }
+
+    /** PARA fechar de vez a thread **/
+    public void closeThread() {
         running = false;
+        estado = Estado.CLOSED;
+        this.interrupt();
     }
-	
-public MovimentosAleatorios() {
-		
-	
-		
-	}
 
+    @Override
+    public void run() {
 
-public void run() {
-	
-	System.out.println("RESULTARRR!!");
-	
-	while(true) {
-		if(running) {
-	
-			
-	
-	
-	try {
-		
-		
-		dados.getGestor().pedirRobot();
-		
-		for (int i = 0; i < dados.getSpinner(); i++) {
-			
-			
-			
-			oneTwoThree = (int)(Math.random() * 3) + 1;
+        estado = Estado.ACTIVE;
+        System.out.println("RESULTARRR!! (Estado = ACTIVE)");
 
-			switch (oneTwoThree) {
-			case 1:
-				
-				distancia = (int)(10 + Math.random() * 41);
-				dados.getRobot().Reta(distancia);
-			
-	            Thread.sleep((long) (Math.abs(distancia / VEL_ROBOT * 1000) + 100));
-	            
-	            
-				break;
-				
-			case 2:
-				
-				raio = (int)(10 + Math.random() * 21);
-				angulo = (int)(20 + Math.random() * 71);
-				dados.getRobot().CurvarEsquerda(raio, angulo);
-				
-				
-                Thread.sleep((long) (raio * Math.toRadians(angulo) / VEL_ROBOT * 1000) + 100);
+        while (running) {
 
-				break;
-			case 3:
-				
-				
-				raio = (int)(10 + Math.random() * 21);
-				angulo = (int)(20 + Math.random() * 71);
-				dados.getRobot().CurvarDireita(raio, angulo);
-				
-				
-                Thread.sleep((long) (raio * Math.toRadians(angulo) / VEL_ROBOT * 1000) + 100);
-				break;
-			}
-			
-			
-			
-		}
-		
-		
-		
-		dados.getRobot().Parar(false);
-		dados.getGestor().devolverRobot();
-        Thread.sleep(100);
+            if (estado != Estado.ACTIVE) {
+                // Paused state
+                try { Thread.sleep(100); } 
+                catch (InterruptedException e) {}
+                continue;
+            }
 
-	} catch (InterruptedException e) {
-        Thread.currentThread().interrupt();
-        System.err.println("Thread do robô interrompida: " + e.getMessage());
-    } catch (Exception e) {
-        System.err.println("Erro no processamento do comando: " + e.getMessage());
-        e.printStackTrace();
-    } finally {
-    	
-    	
-        
-        try {
-        } catch (Exception e) {
-            System.err.println("Erro ao liberar semáforo em escrever: " + e.getMessage());
+            try {
+                dados.getGestor().pedirRobot();
+
+                for (int i = 0; i < dados.getSpinner(); i++) {
+
+                    if (estado != Estado.ACTIVE) break;
+
+                    oneTwoThree = (int)(Math.random() * 3) + 1;
+
+                    switch (oneTwoThree) {
+
+                        case 1:
+                            distancia = (int)(10 + Math.random() * 41);
+                            dados.getRobot().Reta(distancia);
+                            Thread.sleep((long)(Math.abs(distancia / VEL_ROBOT * 1000) + 100));
+                            break;
+
+                        case 2:
+                            raio = (int)(10 + Math.random() * 21);
+                            angulo = (int)(20 + Math.random() * 71);
+                            dados.getRobot().CurvarEsquerda(raio, angulo);
+                            Thread.sleep((long)(raio * Math.toRadians(angulo) / VEL_ROBOT * 1000) + 100);
+                            break;
+
+                        case 3:
+                            raio = (int)(10 + Math.random() * 21);
+                            angulo = (int)(20 + Math.random() * 71);
+                            dados.getRobot().CurvarDireita(raio, angulo);
+                            Thread.sleep((long)(raio * Math.toRadians(angulo) / VEL_ROBOT * 1000) + 100);
+                            break;
+                    }
+                }
+
+                dados.getRobot().Parar(false);
+                dados.getGestor().devolverRobot();
+                Thread.sleep(100);
+
+            } catch (InterruptedException e) {
+                if (estado == Estado.CLOSED) break;
+                System.err.println("Thread interrompida: " + e.getMessage());
+            } catch (Exception e) {
+                System.err.println("Erro: " + e.getMessage());
+                e.printStackTrace();
+            }
         }
+
+        System.out.println("Thread MovimentosAleatorios terminada (Estado = " + estado + ")");
     }
-	
-	
-		}
-	}
-	
-	
-	
-	
-}
-
-	
-public void setDistancia(int distance) {
-	this.distancia = distance;
-}
-public int getDistancia() {
-	return distancia;
-}
-
-public void setRaio(int radius) {
-	this.raio = radius;
-}
-public int getRaio() {
-	return raio;
-}
-
-public void setAngulo(int angle) {
-	this.angulo = angle;
-}
-public int getAngulo() {
-	return angulo;
-}
-
-public void setOneTwoThree(int unoDosTres) {
-	this.oneTwoThree = unoDosTres;
-}
-public int getOneTwoThree() {
-	return oneTwoThree;
-}
-
-public void setDados(Dados data) {
-	this.dados = data;
-}
-public Dados getDados() {
-	return dados;
-}
-
-	
-	
-
 }
