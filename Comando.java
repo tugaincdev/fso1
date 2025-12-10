39be0e4f79b374f572c016fc65537deacf2b5659
tugaincdev <tugaincdev@gmail.com>
2025-12-10 12:31:31 +0000

    Update Comando.java

--- Comando.java
+++ Comando.java
@@ -3,50 +3,50 @@ import java.util.ArrayList;
 public class Comando {
 	
 	
-	private int arg1, arg2, indent;
+	private int arg1, arg2, argID;
 	private boolean arg1B;
 	private String arg1S;
-	public static int ID_Reta = 1;
-	public static int ID_CurvarEsquerda = 2;
-	public static int ID_CurvarDireita = 3;
-	public static int ID_Parar = 0;
-	public static int ID_LigarID = 4;
-	public static int ID_OpenEV3 = 5;
-	public static int ID_CloseEV3 = 6;
+	public static final int ID_Reta = 1;
+	public static final int ID_CurvarEsquerda = 2;
+	public static final int ID_CurvarDireita = 3;
+	public static final int ID_Parar = 0;
+	public static final int ID_LigarID = 4;
+	public static  final int ID_OpenEV3 = 5;
+	public static final int ID_CloseEV3 = 6;
 
 	
 	
 	public Comando( int argID, int arg1, int arg2) {
-		this.indent = argID;
+		this.argID = argID;
 		this.arg1 = arg1;
 		this.arg2 = arg2;
 	}
     public Comando(int argID, int arg1) {
-		this.indent = argID;
+		this.argID = argID;
 		this.arg1 = arg1;	
 	}
     
     public Comando(int argID, boolean arg1) {
-		this.indent = argID;
+		this.argID = argID;
 		this.arg1B = arg1;
 	}
     
     public Comando(int argID, String arg1) {
-		this.indent = argID;
+		this.argID = argID;
 		this.arg1S = arg1;	
 	}
     
     public Comando(int argID) {		
-		this.indent = argID;		
+		this.argID = argID;		
 	}
     
     
     public int getArgID() {
-    	return indent;
+    	return argID;
     }
     
     public void setID(int newArgID) {
-    	indent = newArgID;
+    	argID = newArgID;
     }
     
 	public int getArg1() {
@@ -58,11 +58,11 @@ public class Comando {
 	}
 	
 	public int getArg2() {
-		return arg1;
+		return arg2;
 	}
 	
 	public void setArg2(int newArg2) {
-		arg1 = newArg2;
+		arg2 = newArg2;
 	} 
 	
 	public boolean getArg1B() {
@@ -72,6 +72,18 @@ public class Comando {
 	public void setArg1B(boolean newArg1B) {
 		arg1B = newArg1B;
 	} 
+	
+	@Override
+	public boolean equals(Object o) {
+	    if (this == o) return true;
+	    if (!(o instanceof Comando)) return false;
+	    Comando c = (Comando) o;
+	    return argID == c.argID &&
+	           arg1 == c.arg1 &&
+	           arg2 == c.arg2 &&
+	           arg1B == c.arg1B;
+	}
+
     
 
 }
